c47cbd6a9a7efde16a11cae6764f687263a30ac6
Radenihbos <113280187+Radenihbos@users.noreply.github.com>
2022-10-07 18:12:05 +0700

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -6,6 +6,8 @@
 # Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
+# by @Radenihboss
+# t.me/Jomblosuport & t.me/Jombloproject
 
 import os
 import sys
